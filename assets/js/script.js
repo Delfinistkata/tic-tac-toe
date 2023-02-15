03cfba93e72cf3291db50bc7e1bb42a5c9160b2e
Delfinistkata <elidimitrovadkit@gmail.com>
2023-02-15 21:49:03 +0000

    Add player's mode, add function for players mode when selected

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,29 @@
+// Code from: https://www.tutorialstonight.com/tic-tac-toe-javascript //
+// Selecting all elements //
+
+const player1 = document.getElementById('player1');
+const player2 = document.getElementById('player2');
+
+
+var Player1 = "";
+var Player2 = "";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Pop up message when you click on New game //
 
 function openPopup() {
@@ -8,6 +34,19 @@ window.onload = function () {
     openPopup();
 }
 
-function closePopup() {
+
+function closePopup(options) {
+    if (options === "X") {
+        Player1 = "X";
+        Player2 = "O";
+        player1.innerHTML = 'Player 1: X';
+        player2.innerHTML = 'Player 2: O';
+
+    } else {
+        Player1 = "O";
+        Player2 = "X";
+        player1.innerHTML = 'Player 1: O';
+        player2.innerHTML = 'Player 2: X';
+    }
     document.getElementById("pop-up").style.display = "none";
 }
