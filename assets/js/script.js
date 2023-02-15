1b1b0b4f66d0d54c991e7afc925b15f6fcf8d2b4
Delfinistkata <elidimitrovadkit@gmail.com>
2023-02-15 16:27:43 +0000

    Add three buttons to game page

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+//Code from: https://codepen.io/groundtutorial/pen/gOjKeOJ //
+
+const player = "X";
+const computer = "O";
+
+let board_full = false;
+let play_board = ["", "", "", "", "", "", "", "", ""];
